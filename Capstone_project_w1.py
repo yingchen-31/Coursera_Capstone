455c3f1eb98c21e8097a80d993266aa32c7b17f6
YC L <yingchen_liu@hotmail.com>
2020-11-13 17:00:32 +0800

    update python verson in atom and create a new py file to run the code

--- Capstone_project_w1.py
+++ Capstone_project_w1.py
@@ -1,4 +1,4 @@
 # ## This notebook will be mainly used for the IBM's Applied Data Science Capstone Project
 import pandas as pd
-import numpy  as np
+import numpy as np
 print("Hello Capstone Project Course!")
